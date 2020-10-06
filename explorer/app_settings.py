da052a142fb43141b42bb7cf79db08540407550d
pc-info <t-vitor@iabs.org.br>
2020-10-06 19:36:18 -0300

    Autodiscover para todos os apps nas configurações do celery

--- explorer/app_settings.py
+++ explorer/app_settings.py
@@ -67,6 +67,6 @@ S3_ACCESS_KEY = getattr(settings, "EXPLORER_S3_ACCESS_KEY", None)
 S3_SECRET_KEY = getattr(settings, "EXPLORER_S3_SECRET_KEY", None)
 S3_BUCKET = getattr(settings, "EXPLORER_S3_BUCKET", None)
 FROM_EMAIL = getattr(settings, 'EXPLORER_FROM_EMAIL', 'django-sql-explorer@example.com')
-BASE_TEMPLATE = getattr(settings, 'EXPLORER_EMAIL_BASE_TEMPLATE', None)
+EMAIL_BASE_TEMPLATE = getattr(settings, 'EXPLORER_EMAIL_BASE_TEMPLATE', None)
 
 UNSAFE_RENDERING = getattr(settings, "EXPLORER_UNSAFE_RENDERING", False)
