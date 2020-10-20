b9cad5fb8c27b91c55414c8b7f8438c59a5d3bbc
pc-info <t-vitor@iabs.org.br>
2020-10-20 16:23:34 -0300

    Usando codificação default para conteúdo de arquivo subido ao aws

--- explorer/tasks.py
+++ explorer/tasks.py
@@ -49,7 +49,7 @@ def execute_query(query_id, email_address):
     try:
         output_file = exporter.get_file_output()
         output_file.seek(0)
-        url = s3_upload('%s.csv' % q.title.replace(' ', '_'), bytes(output_file.read(),'iso-8859-1').decode('utf-8'))
+        url = s3_upload('%s.csv' % q.title.replace(' ', '_'), BytesIO(output_file.read().encode('iso-8859-1', 'ignore').decode('utf-8')))
 
         if app_settings.EMAIL_BASE_TEMPLATE:
             email_content = get_template(
